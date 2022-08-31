01991bc9632bd445cf1576a8ef3da723bec8624a
iwankk <iwankartiwan77@gmail.com>
2022-08-31 23:40:57 +0700

    Done

---
+++ Day05/Owner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    internal class Owner : Person
+    {
+        private int hasCompany;
+        public Owner(string firstName, string lastName, string email, DateTime birthDay, int hasCompany) : base(firstName, lastName, email, birthDay)
+        {
+            SocialNumber = new Random().Next(1000, 1100);
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDay = birthDay;
+            this.hasCompany = hasCompany;
+        }
+
+        public int HasCompany { get => hasCompany; set => hasCompany = value; }
+
+        public override string? ToString()
+        {
+            return $"{base.ToString()}|hasCompany : {this.hasCompany}";
+        }
+
+        public override decimal TotalIncome(Dictionary<string, decimal> data)
+        {
+            var totalIncome = 0M;
+            foreach (var item in data)
+            {
+                totalIncome += item.Value * hasCompany;
+            }
+
+            return totalIncome;
+            //throw new NotImplementedException();
+        }
+    }
+}
--- Day05/Program.cs
+++ Day05/Program.cs
@@ -4,10 +4,41 @@ using Day05;
 Person yuli = new Customer("Faiz", "Senpai", "senpai@gmail.com", new DateTime(1998, 4, 24), "121-001");
 Customer widi = new Customer("Widi", "Dios", "Wios@gmail.com", new DateTime(1997, 12, 2), "121-002");
 Customer wido = new Customer("Wido", "Dios", "Wios12@gmail.com", new DateTime(1999, 11, 2), "121-003");
+Owner Ow1 = new Owner("Iwan", "Kartiwan", "gggaming@gmail.com",new DateTime(1998,12,05),5);
+Owner Ow2 = new Owner("Rian", "Pante", "gggaming@gmail.com", new DateTime(1998, 11, 05), 2);
+Owner Ow3 = new Owner("Faiz", "HHH", "gggaming@gmail.com", new DateTime(1998, 10, 05), 7);
 
 var listOfCustomer = new List<Person> { yuli, widi, wido };
+var listOfOwner = new List<Owner> { Ow1,Ow2, Ow3 };
 
-var incomeYuli = new Dictionary<string, decimal>
+var incomeOw1 = new Dictionary<string, decimal>
+{
+    {"PT. ABC",20_000_000 },
+    {"PT. AAV",50_000_000 },
+};
+
+Ow1.TotalRevenue = Ow1.TotalIncome(incomeOw1);
+
+
+var incomeOw2 = new Dictionary<string, decimal>
+{
+    {"PT. LKS",30_000_000},
+    {"PT. DIOS",50_000_000},
+};
+
+Ow2.TotalRevenue = Ow2.TotalIncome(incomeOw2);
+
+var incomeOw3 = new Dictionary<string, decimal>
+{
+    {"PT Hayam",150_000_000},
+    {"PT. SABUBUKNA",50_000_000},
+};
+Ow3.TotalRevenue = Ow3.TotalIncome(incomeOw3);
+
+
+
+
+/*var incomeYuli = new Dictionary<string, decimal>
 {
     {"Kontrakan", 10_000_000 },
     {"Toko",100_000_000 },
@@ -30,16 +61,60 @@ var incomeWido = new Dictionary<string, decimal>
 };
 
 wido.TotalRevenue = wido.TotalIncome(incomeWido);
-
-//yuli.TotalIncome()
-foreach (var item in listOfCustomer)
+*/
+foreach (var item in listOfOwner)
 {
     WriteLine(item.ToString());
 }
+
+WriteLine("------- Revenue Minimum -------");
+var listMin = listOfOwner.Min(C => C.TotalRevenue);
+WriteLine($"Owner Min Revenue : {listMin}");
+
+WriteLine("------- List Range >= 50.000.0000 -------");
+var listRange = listOfOwner.Where(C => C.TotalRevenue >= 50_000_000).ToList();
+foreach (var item in listRange)
+{
+    WriteLine(item.ToString());
+}
+
+WriteLine("------- Revenue Maximum -------");
+var listMax = listOfOwner.Max(C => C.TotalRevenue);
+WriteLine($"Owner Max Revenue : {listMax}");
+
+
+var listLessThanMax = listOfOwner.Where(C => C.TotalRevenue < listMax);
+WriteLine("------- Revenue Lower Than Max -------");
+foreach (var item in listLessThanMax)
+{
+    WriteLine(item.ToString());
+}
+
+WriteLine("------- Select -------");
+var query = listOfOwner.Select(cust =>
+    new
+    {
+        FullName = cust.FirstName + " " + cust.LastName,
+        Email = cust.Email,
+        TotalRevenue = cust.TotalRevenue
+    }
+);
+
+foreach (var item in query)
+{
+    WriteLine(item);
+}
+
+
+//yuli.TotalIncome()
+/*foreach (var item in listOfCustomer)
+{
+    WriteLine(item.ToString());
+}*/
 //LinQ Call
 //LinQ.IntroLinq();
 
-WriteLine("------- List Range >= 50.000.0000 -------");
+/*WriteLine("------- List Range >= 50.000.0000 -------");
 var listRange = listOfCustomer.Where(C => C.TotalRevenue >= 50_000_000).ToList();
 foreach (var item in listRange)
 {
@@ -76,4 +151,5 @@ foreach (var item in query)
 {
     WriteLine(item);
 }
-
+*/
+ReadLine();
